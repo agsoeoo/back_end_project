91f9fbe17bb689a9edf56fc73e7f991acead549b
oo <aungoosoe@gmail.com>
2020-11-19 17:18:37 -0700

    13.2 11/19 axios, sign Up

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import axios from 'axios'
 import router from './routes.js'
 
+axios.defaults.baseURL = "https://cis410-fa20-oo-api.azurewebsites.net";
 new Vue({
   el: '#app',
   router,
