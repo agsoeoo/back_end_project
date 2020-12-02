213732303beba438b0c3785249d2b61e789b7f94
oo <aungoosoe@gmail.com>
2020-12-01 18:42:25 -0700

    15.1 12/1 deployment prep

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
-
 import store from './store.js'
-
-
 import router from './routes.js'
+
 
 axios.defaults.baseURL = "https://cis410-fa20-oo-api.azurewebsites.net";
 new Vue({
   el: '#app',
   router,
-  store: store,
+  store,
   render: h => h(App)
 })
